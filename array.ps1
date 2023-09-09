0ca86e91df01c3988dd6c901c5d3afe48a5a2720
jviblack <javie.couto.dominguez@gmail.com>
2023-09-09 14:32:55 +0200

    commit

---
+++ array.ps1
@@ -0,0 +1,9 @@
+$array_vacio = @()
+$enteros = @(1,2,3,4,5,6)
+$enteros = 1,2,3,4,5,6
+$consecutivos = 1..10
+$caracteres = 'a','b','c','d'
+$multi_tipo = @(1,2,3,'a','b')
+
+Write-Host $array_vacio $enteros $caracteres $multi_tipo
+Write-Host $consecutivos
---
+++ op logicos.ps1
@@ -0,0 +1,16 @@
+#Valores boolenos True o False, 1 o 0
+
+write-host ((5 -gt 1) -And (5 -lt 10)) #ge mayor que; -le menor que; -gt mayor o igual que; -lt menor o igual que
+write-host ((5 -gt 1) -And (5 -lt 1))
+''
+write-host ((5 -gt 1) -or (5 -lt 10))
+write-host ((5 -gt 1) -or (5 -lt 1))
+''
+write-host ((5 -gt 1) -xor (5 -lt 10))
+write-host ((5 -gt 1) -xor (5 -lt 1))
+''
+Write-Host (-Not 5 -gt 1)
+''
+$hola = 'hola'
+$hola -is [int]
+$hola -isnot [int]
