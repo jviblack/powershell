b39a40d4f09f29566da7930254b3022ea97d661c
jviblack <javie.couto.dominguez@gmail.com>
2023-09-17 19:59:46 +0200

    commit

---
+++ exchange/5.firmadigital.ps1
@@ -0,0 +1,6 @@
+#desde admin ir a exchange, permisos, directivas de outlook,
+#quitar caracteristica firma de e-mail
+#en flujo de correo, reglas, añadimos aplicar declinación de responsabilidades,
+#aplicar con remitente dentro de la organizacion
+#anexar aviso añadiendo el html de la firma digital variables entre %% %% y encapsular
+#prioridad 1
---
+++ exchange/6.antispam.ps1
@@ -0,0 +1,3 @@
+#desde admin ir a exchange, e ir a security desde admin center,
+#police and rules, threat policies, anti-spam,
+#anti-span inbound/outbound, umbral ideal 3-4, 
