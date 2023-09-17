42418ed9e4beaea6a946c04d43f2a79d4b5ac8e4
jviblack <javie.couto.dominguez@gmail.com>
2023-09-17 19:24:24 +0200

    commit

---
+++ exchange/3.omitirspam.ps1
@@ -0,0 +1,3 @@
+#desde admin center en office 365 e ir a exchange, flujo de correo, reglas y añadir
+#omitir filtro de correo no deseado aplicando rango de IP's
+#poner en prioridad alta
---
+++ exchange/4.archivadolocal.ps1
@@ -0,0 +1,3 @@
+#desde admin ir a exchange, destinatarios, buzones
+#seleccionar usuario, en mas acciones administrar archivo de buzon,
+#habilitar
