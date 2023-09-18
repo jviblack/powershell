fd8015316bf5bcf0f90ad7d57998151a7a013877
jviblack <javie.couto.dominguez@gmail.com>
2023-09-18 21:15:34 +0200

    commit

---
+++ exchange/7.seguimientocorreos.ps1
@@ -0,0 +1,3 @@
+#desde admin ir a exchange, flujo de correo, seguimiento de mensaje,
+#busquedas personalizadas, empezar seguimiento, en tipo de reporte se pueden exportar,
+#se pueden ver los saltos del correo y buscar correos mediante id o ip desde las queries,
