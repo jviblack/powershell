01ad4d882e34f218b627ea5f9a72d37215c902d5
jviblack <javie.couto.dominguez@gmail.com>
2023-09-18 22:20:53 +0200

    commit

--- exchange/8.archivadolocalpowershell.ps1
+++ exchange/8.archivadolocalpowershell.ps1
@@ -1,7 +1,7 @@
 Connect-ExchangeOnline
 
-$mailboxes = Import-Csv "ruta"
+$mailboxes = Import-Csv "ruta archivo de usuarios" #en un bloc EmailAddress y escribir correo hacia abajo
 
 foreach ($mailbox in $mailboxes){
-    
+    Enable-Mailbox -Identity $mailbox.EmailAddress -Archive
 }
